63c551640d304d10b79244522299c385afa63ba7
Zubenna <emelunna@gmail.com>
2022-08-05 10:50:00 +0100

    Adding backend files

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const connectDB = require('./config/db');
+// const session = require("express-session");
+// const path = require("path");
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+const productRoute = require('./routes/products');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config('.env');
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// dotenv.config(".env");
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+// app.use(express.static("uploads"));
+// app.use(cors());
+// app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.options('*', cors());
+app.use('/api/user', userRoute);
+app.use('/api/product', productRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/order', orderRoute);
+
+// app.get('/', (req, res) => {
+//   res.send('Testing my API ');
+// });
+
+// app.get('/url', (req, res) => {
+//   res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
+// });
+
+app.listen(6050, () => {
+  console.log(`app is listening on port ${process.env.PORT}`);
+});
+
+module.exports = app;
---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Mongo DB conected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log('database connection failed. exiting now...');
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
