ce5b0b043db394eec15924a89f95fe55d80dc74c
Zubenna <emelunna@gmail.com>
2022-09-14 07:12:46 +0100

    Updated app.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
-require('dotenv').config('.env');
+
 const login = asyncHandler(async (req, res) => {
   // Get user input
   const { email, password } = req.body;
--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { logEvents } = require('./middleware/logger');
 const app = require('./app');
-require('dotenv').config('.env');
+
 // const app = require("./app");
 // app.listen(process.env.PORT || 8080, () => {
 //     console.log(`app is listening on port ${process.env.PORT}`)
